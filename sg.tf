bc6f2618a00d0e82fd0fe227065c0a718af7faa1
Thudum Rakesh <144659414+thudumrakesh@users.noreply.github.com>
2024-03-04 23:05:15 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "sg1" {
+  vpc_id = aws_vpc.v1.id
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "python"
+  }
+}
