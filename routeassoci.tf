17adc0a72d3f2985330ce63cf83f711e08278bdc
Thudum Rakesh <144659414+thudumrakesh@users.noreply.github.com>
2024-03-05 14:56:51 +0530

    Update routeassoci.tf

--- routeassoci.tf
+++ routeassoci.tf
@@ -2,3 +2,7 @@ resource "aws_route_table_association" "route1" {
   subnet_id      = aws_subnet.publicsubnet1.id
   route_table_id = aws_route_table.route1.id
 }
+route {
+cidr_block ="0.0.0.0/0"
+gateway_id = aws_internet_gateway.IGW.id
+}
