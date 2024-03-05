9de9c94153bf8b6df844ac502a8c0fcf973f425d
Thudum Rakesh <144659414+thudumrakesh@users.noreply.github.com>
2024-03-05 15:19:05 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -2,8 +2,7 @@
 sudo amazon-linux-extras install python3.8
 sudo yum -y update
 sudo yum -y install git
-git clone https://github.com/thudumrakesh/fuel-consumption.git
-cd fuel-consumption
-ls
+git clone https://github.com/thudumrakesh/car-predection.git
+cd car-predection
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
