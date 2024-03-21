ae6f67ac1974c57f4ecf36b3f26289306e386b29
Thudum Rakesh <144659414+thudumrakesh@users.noreply.github.com>
2024-03-21 13:50:16 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "fish" {
-  ami           = "ami-0cf7b2f456cd5efd4"
+  ami           = "ami-02d7fd1c2af6eead0"
   instance_type = "t2.micro"
 
-  key_name                    = "python"
+  key_name                    = "node"
   vpc_security_group_ids      = [aws_security_group.sg1.id]
   subnet_id                   = aws_subnet.publicsubnet1.id
   associate_public_ip_address = true
