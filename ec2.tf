ba22bc3812a1b7437823ad27ffa4b35eec0733ef
Thudum Rakesh <144659414+thudumrakesh@users.noreply.github.com>
2024-03-21 15:34:00 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "fish" {
   vpc_security_group_ids      = [aws_security_group.sg1.id]
   subnet_id                   = aws_subnet.publicsubnet1.id
   associate_public_ip_address = true
-  user_data                   = "${file("data.sh")}"
+  user_data                   = file("data.sh")
   tags = {
     Name = "CAR"
   }
